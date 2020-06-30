d1a068ae99976793df2c98cf9dbb1113e3cc2bed
rija1 <rija.ratinahirana@gmail.com>
2020-06-30 11:43:46 +0545

    stuff

--- wp-content/themes/druponrinpoche/single-online-teaching.php
+++ wp-content/themes/druponrinpoche/single-online-teaching.php
@@ -67,7 +67,7 @@ $regLink = admin_url('admin-ajax.php?action=online_teaching_register&register=1&
         jQuery(".modal_buttons").show();
 
         if(action == 'register') {
-            var modalText = '<?php echo pll__('I confirm I can attend all the sessions for this course ?'); ?>';
+            var modalText = '<?php echo pll__('I confirm I can attend all the sessions for this course.'); ?>';
 
         } else if (action == 'unregister') {
             var modalText = '<?php echo pll__('Do you confirm you want to unregister from this course ? You cannot register again once the registration date limit has passed.'); ?>';
