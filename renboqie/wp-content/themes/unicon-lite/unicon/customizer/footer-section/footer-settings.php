74e8bd6e86fcd6edcdd895b8080fb181c5da6b83
rija1 <rija.ratinahirana@gmail.com>
2021-05-23 14:34:29 +0545

    delete renboqie folder

--- renboqie/wp-content/themes/unicon-lite/unicon/customizer/footer-section/footer-settings.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * Load header panel file
-*/
-require $unicon_lite_customizer_footer_options_file_path = unicon_lite_file_directory('unicon/customizer/footer-section/footer-options.php');
