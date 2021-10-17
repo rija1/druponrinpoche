3dfaec9e63cd42288a48a2694cc8b753b1880de9
rija1 <rija.ratinahirana@gmail.com>
2021-10-17 15:53:38 +0545

    remove head fb update style

--- wp-content/themes/druponrinpoche/header.php
+++ wp-content/themes/druponrinpoche/header.php
@@ -54,17 +54,6 @@ $drConfig = getDrWebsiteConfig();
 </head>
 <body <?php body_class(); ?>>
 
-<div id="fb-root"></div>
-<script>
-(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.0';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
-
 		<div id="content" class="content">
 
         <div class="container">
