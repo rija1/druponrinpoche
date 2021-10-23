9afd43bb81d52c951a2c2b06135cf2391141d13d
rija1 <rija.ratinahirana@gmail.com>
2021-10-23 11:47:43 +0545

    lighten css/js load

--- wp-content/themes/druponrinpoche/functions.php
+++ wp-content/themes/druponrinpoche/functions.php
@@ -1218,6 +1218,14 @@ function dkr_str($string,$return=false) {
     return;
 }
 
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function remove_wp_block_library_css(){
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+} 
+add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );
+
 //
 //add_filter('pre_get_posts','filter_archive_dkr');
 //
