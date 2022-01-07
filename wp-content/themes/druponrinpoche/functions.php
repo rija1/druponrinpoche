f08cb255ea0f77ac98eac3534359eed9bea3fe1a
rija1 <rija.ratinahirana@gmail.com>
2022-01-07 13:19:50 +0545

    functions

--- wp-content/themes/druponrinpoche/functions.php
+++ wp-content/themes/druponrinpoche/functions.php
@@ -155,10 +155,8 @@ if( ! function_exists( 'dkr_enqueue_styles' ) ) {
         wp_enqueue_style( 'modal-css', get_template_directory_uri() . '/assets/css/jquery.modal.min.css', array());
 
         // main style
+        wp_enqueue_style( 'dkr-style', get_stylesheet_directory_uri() . '/style.css', array() );
         // wp_enqueue_style( 'dkr-style', get_stylesheet_directory_uri() . '/style.css?version=1', array() );
-
-        // main style
-       wp_enqueue_style( 'dkr-resp', get_template_directory_uri() . '/assets/css/resp.css', array() );
 
     }
     add_action( 'wp_enqueue_scripts', 'dkr_enqueue_styles' );
