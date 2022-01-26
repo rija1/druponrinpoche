76cea9716b682f34fa5cbbbaa389c81d30ef4888
rija1 <rija.ratinahirana@gmail.com>
2022-01-26 15:45:27 +0545

    remove elastic mail

--- wp-content/plugins/elastic-email-sender/template/t-ees_connecterror.php
+++
@@ -1,4 +0,0 @@
-<div class="connection-error-container">
-    <img src="<?php echo esc_url(plugins_url('/src/img/connect_apikey.png', dirname(__FILE__))) ?>">
-    <p class="ee-p"><?php _e('Please connect to Elastic Email API to start using the plugin.', 'elastic-email-sender') ?></p>
-</div>
